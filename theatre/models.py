709ff59a4b7fe67942e5dce2e3f59c3b5ff4d975
Nick Konkov <kharkivnick@gmail.com>
2026-02-02 13:56:09 +0200

    Create models Actor and Genre

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
