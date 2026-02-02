721ca31e5fb44dae82cbf9e43a8a6b248799592c
Nick Konkov <kharkivnick@gmail.com>
2026-02-02 14:00:43 +0200

    Create model Play

--- theatre/models.py
+++ theatre/models.py
@@ -20,4 +20,19 @@ class Genre(models.Model):
         return self.name
 
 
+class Play(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    genres = models.ManyToManyField(Genre)
+    actors = models.ManyToManyField(Actor)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+
+
 
